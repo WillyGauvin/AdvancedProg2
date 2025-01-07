08b92173949a8028cec9c84ffc69c36c12a86e2c
Will Gauvin <122392617+WillyGauvin@users.noreply.github.com>
2025-01-07 18:19:54 -0500

    first commit

---
+++ Client/targetver.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+// // SDKDDKVer.h
+// Windows 
+// WinSDKVer.h
+#include <SDKDDKVer.h>
---
+++ Engine/pch.h
@@ -0,0 +1,11 @@
+﻿// pch.h: 
+
+#ifndef PCH_H
+#define PCH_H
+
+
+#define WIN32_LEAN_AND_MEAN 
+
+#include "EnginePch.h"
+
+#endif //PCH_H
